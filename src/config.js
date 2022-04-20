aca72fb28cb1366fad2c22bf477712fc424e3e7c
Julian Fleischmann <julian.fleischmann@mr-pixel.de>
2022-04-20 22:52:50 +0200

    Added new Component

--- src/config.js
+++ src/config.js
@@ -1,8 +1,24 @@
 // Config starter code
+import React from 'react';
 import { createChatBotMessage } from "react-chatbot-kit";
+import BotAvatar from "./components/BotAvatar";
 
 const config = {
-  initialMessages: [createChatBotMessage(`Hello world`)]
+  initialMessages: [createChatBotMessage(`Hello world`)],
+  botName: "crazy bot",
+  customComponents: {
+      botAvatar: (props) => <BotAvatar {...props} />
+  },
+  customStyles: {
+    // Overrides the chatbot message styles
+    botMessageBox: {
+      backgroundColor: "purple",
+    },
+    // Overrides the chat button styles
+    chatButton: {
+      backgroundColor: "purple",
+    },
+  }
 }
 
 export default config
