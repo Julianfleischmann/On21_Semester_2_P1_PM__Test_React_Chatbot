60cefc92e65c970914b585ec678ebcaebc8e5340
Julian Fleischmann <julian.fleischmann@mr-pixel.de>
2022-04-20 23:05:30 +0200

    Added Message Parser

--- src/MessageParser.js
+++ src/MessageParser.js
@@ -5,8 +5,17 @@ class MessageParser {
       this.state = state;
     }
   
+    // Der Action Parser versucht zu erkennen, was in der Nachricht ist und entscheidet daraufhin, was damit passiert 
     parse(message) {
-      console.log(message)
+      // console.log(message)
+
+      console.log(this.state)
+
+      const lowercase = message.toLowerCase()
+
+      if(lowercase.includes("hello world")) {
+        console.log("Hi")
+      }
     }
   }
   
